d4d3ed483029814038e24f83bc0c88f40f5aa4e9
flora8 <huaflora23@gmail.com>
2024-06-28 15:17:19 +0100

    Update gspr.py

--- gspr.py
+++ gspr.py
@@ -263,21 +263,25 @@ def Analysis(): # Plotting and data visualisation to analyse user experience sur
 
     with Counts: # User select the x-axis to plot the counts
         xvalue_E = st.selectbox("Please select X-Axis value to calculate the total values", options=excel.columns[1:7])
+        xvalue_E = excel.dropna(subset=[xvalue_E]) # remove nan values from a dataframe
         count_E = excel[xvalue_E].value_counts()
         st.bar_chart(count_E)
         expander_E = st.expander("Count Results")
         expander_E.write(count_E)
 
-
-
-
     with Analysis: # User select the x-axis and y-axis value to plot the analysis data
         xaxis_E = st.selectbox("Please select X-Axis value", options=excel.columns[0:7])
-        yaxis_E = st.selectbox("Please select Y-Axis value", options=excel.columns[1:7])        
+        yaxis_E = st.selectbox("Please select Y-Axis value", options=excel.columns[1:7])
+        xaxis_E = excel.dropna(subset=[xaxis_E]) # remove nan values from a dataframe
+        yaxis_E = excel.dropna(subset=[yaxis_E]) # remove nan values from a dataframe
+        
         plot_E = px.scatter(excel, x=xaxis_E, y=yaxis_E, labels={xaxis_E:yaxis_E}, title="The searched {} by {} results".format(xaxis_E,yaxis_E))
         color_E = st.color_picker("Please select the plot color") # user select the particular color                
         plot_E.update_traces(marker=dict(color=color_E)) # Update the plot color after the user chosen
         st.plotly_chart(plot_E, use_container_width=True) # Display the data
+
+        fig = px.line(x=xaxis_E, y=yaxis_E, title="The searched {} by {} results".format(xaxis_E,yaxis_E))
+        st.plotly_chart(fig)
         
         expander2_E = st.expander("Analysis Results")
         data2_E = excel[[xaxis_E, yaxis_E]].groupby(by=xaxis_E)[yaxis_E].sum()
@@ -302,114 +306,8 @@ def Analysis(): # Plotting and data visualisation to analyse user experience sur
 
 
 #---------------------------------#
-def Prediction():
-    st.header(" :mag: Predictive Model 預測模型")
-    st.markdown("""Prediction response value by using linear regression statistical model""")       
-        
-    def model(df):
-        X = st.selectbox("Please select X-Axis value to calculate the total values", options=df.columns[:7])
-        y = st.selectbox("Please select y-Axis value to calculate the total values", options=df.columns[1:7])
-        X = df.dropna(subset=[X]) # remove nan values from a dataframe
-        y = df.dropna(subset=[y]) # remove nan values from a dataframe
-
-        
-
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=split_size, random_state=seed_number)
-        log_reg = LinearRegression().fit(X_train, y_train) # fit scaled training data and label
-        predictions_train = log_reg.score(X_train, y_train) # fit the data to the model
-        predictions_test = log_reg.score(X_test, y_test) # predict the test data
-
-        st.subheader("2. Table of Model Performance")
-        st.write('Training set')
-        st.write(predictions_train)
-        st.markdown(filedownload(predictions_train, 'training.xlsx'), unsafe_allow_html=True)
-        st.write('Test set')
-        st.write(predictions_test)
-        st.markdown(filedownload(predictions_test, 'test.xlsx'), unsafe_allow_html=True)
-
-        st.subheader('3. Plot of Model Performance (Test set)')
-        with st.markdown('**R-squared**'):
-            # Tall
-            predictions_test["R-Squared"] = [0 if i < 0 else i for i in predictions_test["R-Squared"] ]
-            plt.figure(figsize=(3, 9))
-            sns.set_theme(style="whitegrid")
-            ax1 = sns.barplot(y=predictions_test.index, x="R-Squared", data=predictions_test)
-            ax1.set(xlim=(0, 1))
-        st.markdown(imagedownload(plt,'plot-r2-tall.pdf'), unsafe_allow_html=True)
-        # Wide
-        plt.figure(figsize=(9, 3))
-        sns.set_theme(style="whitegrid")
-        ax1 = sns.barplot(x=predictions_test.index, y="R-Squared", data=predictions_test)
-        ax1.set(ylim=(0, 1))
-        plt.xticks(rotation=90)
-        st.pyplot(plt)
-        st.markdown(imagedownload(plt,'plot-r2-wide.pdf'), unsafe_allow_html=True)
-
-        with st.markdown('**RMSE (capped at 50)**'):
-            # Tall
-            predictions_test["RMSE"] = [50 if i > 50 else i for i in predictions_test["RMSE"] ]
-            plt.figure(figsize=(3, 9))
-            sns.set_theme(style="whitegrid")
-            ax2 = sns.barplot(y=predictions_test.index, x="RMSE", data=predictions_test)
-        st.markdown(imagedownload(plt,'plot-rmse-tall.pdf'), unsafe_allow_html=True)
-            # Wide
-        plt.figure(figsize=(9, 3))
-        sns.set_theme(style="whitegrid")
-        ax2 = sns.barplot(x=predictions_test.index, y="RMSE", data=predictions_test)
-        plt.xticks(rotation=90)
-        st.pyplot(plt)
-        st.markdown(imagedownload(plt,'plot-rmse-wide.pdf'), unsafe_allow_html=True)
-
-        with st.markdown('**Calculation time**'):
-            # Tall
-            predictions_test["Time Taken"] = [0 if i < 0 else i for i in predictions_test["Time Taken"] ]
-            plt.figure(figsize=(3, 9))
-            sns.set_theme(style="whitegrid")
-            ax3 = sns.barplot(y=predictions_test.index, x="Time Taken", data=predictions_test)
-        st.markdown(imagedownload(plt,'plot-calculation-time-tall.pdf'), unsafe_allow_html=True)
-            # Wide
-        plt.figure(figsize=(9, 3))
-        sns.set_theme(style="whitegrid")
-        ax3 = sns.barplot(x=predictions_test.index, y="Time Taken", data=predictions_test)
-        plt.xticks(rotation=90)
-        st.pyplot(plt)
-        st.markdown(imagedownload(plt,'plot-calculation-time-wide.pdf'), unsafe_allow_html=True)
-
-    # Download CSV data
-    def filedownload(df, filename):
-        csv = df.to_csv(index=False)
-        b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-        href = f'<a href="data:file/csv;base64,{b64}" download={filename}>Download {filename} File</a>'
-        return href
-
-    def imagedownload(plt, filename):
-        s = io.BytesIO()
-        plt.savefig(s, format='pdf', bbox_inches='tight')
-        plt.close()
-        b64 = base64.b64encode(s.getvalue()).decode()  # strings <-> bytes conversions
-        href = f'<a href="data:image/png;base64,{b64}" download={filename}>Download {filename} File</a>'
-        return href
-
-    #---------------------------------#
-    # Sidebar - Specify parameter settings
-    with st.sidebar.header('Set Parameters'):
-        split_size = st.sidebar.slider('Data split ratio (% for Training Set)', 0, 90, 80, 1)
-        seed_number = st.sidebar.slider('Set the random seed number', 1, 100, 42, 1)
-
-    #---------------------------------#
-    # Main panel
-    st.subheader('1. Dataset') # Displays the dataset
-    st.markdown('**1.1. Glimpse of User  Survey data**')
-    file = pd.read_excel('Survey.xlsx', usecols="A:G")
-    st.write(file)
-    model(file)
-
-
-
-
-#---------------------------------#
 # Create the sidebar for choosing the specific page
-options = st.sidebar.radio("Pages", options=[":stethoscope: Home", " :star2: GSPR", " :memo: Survey", " :bar_chart: Analysis", " :mag: Prediction"])
+options = st.sidebar.radio("Pages", options=[":stethoscope: Home", " :star2: GSPR", " :memo: Survey", " :bar_chart: Analysis"])
 
 if options == ":stethoscope: Home":
     Home()
@@ -419,8 +317,6 @@ elif options == " :memo: Survey":
     Survey()
 elif options == " :bar_chart: Analysis":
     Analysis()
-elif options == " :mag: Prediction":
-    Prediction()
 
 
 
