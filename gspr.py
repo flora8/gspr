1a376aa20da35cdd8254ccffeb4aca3a32dc5395
flora8 <huaflora23@gmail.com>
2024-07-03 15:37:00 +0100

    Update gspr.py

--- gspr.py
+++ gspr.py
@@ -219,7 +219,7 @@ def Survey(): # Collecting user inputs for later analysis
     def save_gsheets(data): # Authentication
         creds = Credentials.from_service_account_info(st.secrets["gcp_service_account"],scopes=["https://www.googleapis.com/auth/spreadsheets"]) 
         client = gspread.authorize(creds)
-        sheet = client.open_by_url(st.secrets["spreadsheet"])
+        sheet = client.open_by_url(st.secrets["url"])
         worksheet = sheet.get_worksheet(0)
         worksheet.append_row(data) # Append data to the sheet
     
