af4dd6bd0e3688da22defa2c6e450e9b2f14f9e6
flora8 <huaflora23@gmail.com>
2024-07-03 10:32:01 +0100

    Update gspr.py

--- gspr.py
+++ gspr.py
@@ -209,8 +209,9 @@ def Survey(): # Collecting user inputs for later analysis
                 
                 非常感謝您在測試系統後，提供英文或中文的使用經驗供後續分析，而收集的結果數據將顯示在下一頁，供每位參與者了解更多信息。:thought_balloon:
                 """)
-    conn = st.experimental_connection("gsheets", type=GSheetsConnection) # Establishing a gheets data
-    excel = conn.read(worksheet="Survey", usecols=list(range(19)))
+    conn = st.experimental_connection("gsheets", type=GSheetsConnection) # Establishing a google sheets connection
+    excel = conn.read(worksheet="Survey", usecols=list(range(19))) # Fetch existing survey data
+    excel = excel.dropna(how="all") 
     st.dataframe(excel)
 
     col1, col2 = st.tabs(["User Experience Survey", "使用者體驗調查"])
