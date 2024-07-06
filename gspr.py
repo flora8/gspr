bb52c30796d69c9d810adb63c1fd353ec45f006e
flora8 <huaflora23@gmail.com>
2024-07-06 19:35:26 +0100

    Update gspr.py

--- gspr.py
+++ gspr.py
@@ -34,19 +34,7 @@ st.markdown(hide, unsafe_allow_html=True)
 
 
 #---------------------------------#
-# Count page views
-def Pageviews():
-    sum = 0
-    for i in range(0,sum+1):
-        sum = sum+i
-    return [sum] # convert the integers to list type
-pageviews = Pageviews()
-pageviews.append('dummy')
 
-try:
-    st.sidebar.markdown('Page viewed: {} times'.format(len(pageviews)))
-except ValueError:
-    st.sidebar.markdown('Page viewed: {} times'.format(1))
 
 
 
