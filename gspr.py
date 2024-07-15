45439326a377c243d581561af7522ac2b9b77fc6
flora8 <huaflora23@gmail.com>
2024-07-15 12:49:02 +0100

    Update gspr.py

--- gspr.py
+++ gspr.py
@@ -284,14 +284,14 @@ def Survey(): # Collecting user inputs for later analysis
         background = st.selectbox("Please select the business type of your background?", ("", "Academics", "Notified Body (NB)", "Contract Research Organization (CRO)", "Manufacturer", "Importer", "Distributor", "Wholesaler", "Others",))
         role = st.selectbox("Please select your current role?", ("", "Professionals", "Professor", "Student", "Reviewer", "Clinical Research Associate (CRA)", "Manager", "Engineer", "Officer", "Sales Representative", "Assistant", "Others", "Prefer not to say"))
 
-        EMDN_category = st.selectbox("Which EMDN code category of medical device are you particularly interested in searching for on this application?", list(emdn_E_part)) # set index to none means there is no default options
-        group_E = emdn_E_part.groupby(by=[EMDN_category], as_index=False)[[]].sum() # Group the EMDN code type based on the specific category chosen
-        EMDN_type = st.selectbox("Which EMDN code type of medical device are you particularly interested in searching for on this application?", list(group_E.iloc[:,0]))
-        EMDN_category_all = st.selectbox("Which EMDN code category of medical device are you interested in searching for in the future?", list(emdn_E_all)) # set index to none means there is no default options
-        group_E_all = emdn_E_all.groupby(by=[EMDN_category_all], as_index=False)[[]].sum() # Group the EMDN code type based on the specific category chosen
-        EMDN_type_all = st.selectbox("Which EMDN code type of medical device are you interested in searching for in the future?", list(group_E_all.iloc[:,0]))
+        category_E = st.selectbox("Which EMDN code category of medical device are you particularly interested in searching for on this application?", list(emdn_E_part)) # set index to none means there is no default options
+        group_E = emdn_E_part.groupby(by=[category_E], as_index=False)[[]].sum() # Group the EMDN code type based on the specific category chosen
+        type_E = st.selectbox("Which EMDN code type of medical device are you particularly interested in searching for on this application?", list(group_E.iloc[:,0]))
+        category_E_all = st.selectbox("Which EMDN code category of medical device are you interested in searching for in the future?", list(emdn_E_all)) # set index to none means there is no default options
+        group_E_all = emdn_E_all.groupby(by=[category_E_all], as_index=False)[[]].sum() # Group the EMDN code type based on the specific category chosen
+        type_E_all = st.selectbox("Which EMDN code type of medical device are you interested in searching for in the future?", list(group_E_all.iloc[:,0]))
         
-        information = st.selectbox("How would you rate the provided device information on this website overall?", ("","1: Absolutely appropriate and clear", "2: Appropriate and clear", "3: Neutral", "4: Inappropriate and unclear", "5: Absolutely inappropriate and unclear"))
+        information = st.selectbox("How would you rate the provided device information on this website application overall?", ("","1: Absolutely appropriate and clear", "2: Appropriate and clear", "3: Neutral", "4: Inappropriate and unclear", "5: Absolutely inappropriate and unclear"))
         experience = st.selectbox("How would you rate the benefits of having the regulation decision website application?", ("","1: Extremely useful and meaningful", "2: Useful and meaningful", "3: Neutral", "4: Useless and meaningless", "5: Extremely useless and meaningless"))
         others = st.text_area("What other information would you like to see on this page? (Optional)")
         feedback = st.text_area("Do you have any additional comments, concerns, feedback, or suggestions on this system that we could improve? (Optional)")
@@ -300,7 +300,7 @@ def Survey(): # Collecting user inputs for later analysis
         if submit == True: # if the submit button is pressed
             st.success("Successfully submitted. !! Thank you so much for your support !! ")       
             sheet = client.open_by_url(url).worksheet('survey')  # Access the Google Sheet
-            data = [day,background,role,EMDN_category,EMDN_type,information,experience,others,feedback] # Read data from the user input
+            data = [day,background,role,category_E,type_E,category_E_all,type_E_all,information,experience,others,feedback] # Read data from the user input
             sheet.append_row(data) # Append data to the Google sheet
 
 
@@ -310,9 +310,12 @@ def Survey(): # Collecting user inputs for later analysis
         background_C = st.selectbox("請問您的背景", ("", "學術單位", "驗證機構(NB)", "受託研究機構(CRO)", "製造商", "進口商", "經銷商", "其他",))
         role_C = st.selectbox("請問您目前的職位", ("", "專業人士", "教授", "學生", "審查員", "臨床試驗人員", "經理", "工程師", "專員", "業務", "助理", "其他", "不方便提供"))               
     
-        EMDN_category_C = st.selectbox("應用程式顯示的資訊，請問您對哪種 EMDN 分類的醫療器材特別感興趣搜尋?", list(emdn_C_part)) # set index to none means there is no default options
-        group_C = emdn_C_part.groupby(by=[EMDN_category_C], as_index=False).sum() # Group the EMDN code type based on the specific category chosen
-        EMDN_type_C = st.selectbox("請問您對哪種 EMDN 類型的醫療器材特別感興趣搜尋?", list(group_C.iloc[:,0]))
+        category_C = st.selectbox("應用程式顯示的資訊，請問您對哪種 EMDN 分類的醫療器材特別感興趣搜尋?", list(emdn_C_part)) # set index to none means there is no default options
+        group_C = emdn_C_part.groupby(by=[category_C], as_index=False).sum() # Group the EMDN code type based on the specific category chosen
+        type_C = st.selectbox("請問您對哪種 EMDN 類型的醫療器材特別感興趣搜尋?", list(group_C.iloc[:,0]))
+        category_C_all = st.selectbox("Which EMDN code category of medical device are you interested in searching for in the future?", list(emdn_C_all)) # set index to none means there is no default options
+        group_C_all = emdn_C_all.groupby(by=[category_C_all], as_index=False)[[]].sum() # Group the EMDN code type based on the specific category chosen
+        type_C_all = st.selectbox("Which EMDN code type of medical device are you interested in searching for in the future?", list(group_C_all.iloc[:,0]))
 
         information_C = st.selectbox("請問您對本網站所提供的整體醫材資訊評價如何？", ("","1: 非常適當和明確", "2: 適當和明確", "3: 普通", "4: 不適當和不明確", "5: 非常不適當和不明確"))
         experience_C = st.selectbox("請問您對使用監管決策網站的優勢有何評價？", ("","1: 非常有幫助和有意義", "2: 有幫助和有意義", "3: 普通", "4: 無幫助和無意義", "5: 非常無幫助和無意義"))
@@ -323,7 +326,7 @@ def Survey(): # Collecting user inputs for later analysis
         if submit_C == True: # if the submit button is pressed
             st.success("提交成功 !! 非常感謝您寶貴的意見及支持 !! ")      
             sheet_C = client.open_by_url(url).worksheet('調查')  # Access the Google Sheet
-            data_C = [day_C,background_C,role_C,EMDN_category_C,EMDN_type_C,information_C,experience_C,others_C,feedback_C] # Read data from the user input
+            data_C = [day_C,background_C,role_C,category_C,type_C,category_C_all,type_C_all,information_C,experience_C,others_C,feedback_C] # Read data from the user input
             sheet_C.append_row(data_C) # Append data to the Google sheet    
             
      
