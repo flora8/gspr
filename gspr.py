a6ebe82bd41bd0bc4bb219971bb7bbc266317aff
flora8 <huaflora23@gmail.com>
2024-07-15 01:17:26 +0100

    Update gspr.py

--- gspr.py
+++ gspr.py
@@ -126,13 +126,13 @@ def EMDN(): # Create the EMDN page
         group_E = emdn_E_part.groupby(by=[category_E], as_index=False)[[]].sum() # Group the EMDN code type based on the specific category chosen
         type_E = st.selectbox("Please select the EMDN code type", list(group_E.iloc[:,0])) # List each EMDN code type so the user can select which medical device to search for 
         
-        if st.button("Search"): # Set up the button
+        if st.button("Search") # Set up the button
             st.success("Please wait a few minutes; the page turns on medical device: {} information".format(type_E))
             st.write("The {} information shown can be searched, fullscreen, and downloaded as an Microsoft Excel file for personal records and edits".format(type_E))
             type_E = type_E.split()[0]  # Split the string of EMDN type into a list and return the first element, which has the same name as the Excel worksheet
             GSPR_E(type_E) # The EMDN type will retun to the GSPR_E function
-            else:
-                st.error('The medical device information is unavailable for search; please select another EMDN code type', icon="🚨")
+        elif st.button("Search") is None:
+            st.error('The medical device information is unavailable for search; please select another EMDN code type', icon="🚨")
             
 
     with col2:  # Create the EMDN page in Mandarin
