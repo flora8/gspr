358cf22f970412f1b87a40020ba729946befcaf2
flora8 <huaflora23@gmail.com>
2024-07-30 21:04:18 +0100

    Update gspr.py

--- gspr.py
+++ gspr.py
@@ -348,14 +348,14 @@ def Survey(): # Collecting user inputs for later analysis
         groupby_C = emdn_C_part.groupby(by=[category_C], as_index=False).sum() # Group the EMDN code type based on the specific category chosen
         group_C = st.selectbox("對於網站上可用的搜尋信息，請問您有興趣查看哪個醫療器材的EMDN代碼類群？", list(groupby_C.iloc[:,0]))
         expectation_C = st.selectbox("請問本網站系統輸出的醫療器材資訊內容是否符合您的預期？", ("", "是", "否"))
-        information_C = st.selectbox("請問您對本網站所提供的整體醫材資訊評價如何？", ("","1: 非常適當和明確", "2: 適當和明確", "3: 普通", "4: 不適當和不明確", "5: 非常不適當和不明確"))
-        experience_C = st.selectbox("請問您對使用監管應用網站的優勢有何評價？", ("","1: 非常有幫助", "2: 有幫助", "3: 普通", "4: 無幫助", "5: 非常無幫助"))
+        information_C = st.selectbox("請問您對本應用網站所提供的整體醫材資訊評價如何？", ("","1: 非常適當和明確", "2: 適當和明確", "3: 普通", "4: 不適當和不明確", "5: 非常不適當和不明確"))
+        experience_C = st.selectbox("請問您如何看待使用監管工具應用網站的好處？", ("","1: 非常有幫助", "2: 有幫助", "3: 普通", "4: 無幫助", "5: 非常無幫助"))
 
         category_C_all = st.selectbox("請問您未來特別感興趣搜尋哪種EMDN代碼類別的醫療器材？", list(emdn_C_all)) # set index to none means there is no default options
         groupby_C_all = emdn_C_all.groupby(by=[category_C_all], as_index=False)[[]].sum() # Group the EMDN code type based on the specific category chosen
         group_C_all = st.selectbox("請問您未來特別感興趣搜尋哪種EMDN代碼類群的醫療器材？", list(groupby_C_all.iloc[:,0]))
         
-        others_C = st.text_area("請問您希望在此網站上看到哪些其他資訊？")
+        others_C = st.text_area("請問您希望在此監管工具應用網站上看到哪些其他資訊？")
         feedback_C = st.text_area("請問您對於此系統有任何意見、疑慮、回饋或建議可以幫助我們改進嗎？")
         
         st.write("通過提交回覆，請確認您已閱讀並理解所提供的研究相關資訊，並同意您的匿名數據用於研究目的")
