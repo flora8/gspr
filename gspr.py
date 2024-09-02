0d25795f35abb537f6ae21d445fbfe6f664dc12c
flora8 <huaflora23@gmail.com>
2024-09-02 21:13:08 +0100

    Update gspr.py

--- gspr.py
+++ gspr.py
@@ -402,7 +402,7 @@ def Analysis(): # Plotting and data visualisation to analyse user experience sur
         count_E = data_E[xvalue_E].value_counts().reset_index()
         selected = data_E[xvalue_E]
         #fig2_E = px.pie(count_E, values=xvalue_E, title="Pie chart: {} distribution".format(xvalue_E)) # Display the distribution of species in the data
-        fig2_E = px.pie(count_E, values=xvalue_E, names= selected.index, title="Pie chart: {} distribution".format(xvalue_E)) # Display the distribution of species in the data
+        fig2_E = px.pie(count_E, values=xvalue_E, names="xvalue_E.index", title="Pie chart: {} distribution".format(xvalue_E)) # Display the distribution of species in the data
         st.plotly_chart(fig2_E)
 
         
