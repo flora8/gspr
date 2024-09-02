6568eb8f191b550428de9544af7cf502d6cf89c3
flora8 <huaflora23@gmail.com>
2024-09-02 21:57:39 +0100

    Update gspr.py

--- gspr.py
+++ gspr.py
@@ -400,7 +400,7 @@ def Analysis(): # Plotting and data visualisation to analyse user experience sur
 
     with Counts: # User select the x-axis to plot the counts
         xvalue_E = st.selectbox("Please select X-Axis value to calculate the total values", options=data_E.columns[0:5])
-        selected = data_E[xvalue_E].index.tolist()
+        selected = data_E.index([xvalue_E])
         count_E = data_E[xvalue_E].value_counts().reset_index()
         #count_E = data_E[xvalue_E].value_counts()
         
