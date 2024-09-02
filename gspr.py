766a852a3d1b21c9075929e3cc232d54d754c852
flora8 <huaflora23@gmail.com>
2024-09-02 21:55:38 +0100

    Update gspr.py

--- gspr.py
+++ gspr.py
@@ -400,9 +400,10 @@ def Analysis(): # Plotting and data visualisation to analyse user experience sur
 
     with Counts: # User select the x-axis to plot the counts
         xvalue_E = st.selectbox("Please select X-Axis value to calculate the total values", options=data_E.columns[0:5])
+        selected = data_E[xvalue_E].index.tolist()
         count_E = data_E[xvalue_E].value_counts().reset_index()
         #count_E = data_E[xvalue_E].value_counts()
-        selected = data_E[xvalue_E].index.tolist()
+        
         #fig2_E = px.pie(count_E, values=xvalue_E, title="Pie chart: {} distribution".format(xvalue_E)) # Display the distribution of species in the data
         fig2_E = px.pie(count_E, values=xvalue_E, names=selected, title="Pie chart: {} distribution".format(xvalue_E)) # Display the distribution of species in the data
         st.plotly_chart(fig2_E)
